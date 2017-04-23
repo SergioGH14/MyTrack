1500c2ad1c6f0c127d77c2c957b596655166492a
Ines <agnes19gomez@gmail.com>
2017-04-23 18:10:03 +0200

    vista profile web, vista profile usuario, vista generos

---
+++ app/src/main/java/com/sdm/sergio/mytrack/adapter/GenreListAdapter.java
@@ -0,0 +1,40 @@
+package com.sdm.sergio.mytrack.adapter;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sdm.sergio.mytrack.R;
+
+public class GenreListAdapter extends ArrayAdapter {
+
+    String[] androidListViewStrings;
+    Integer[] imagesId;
+    Context context;
+
+    public GenreListAdapter(Activity context, Integer[] imagesId, String[] textListView) {
+        super(context, R.layout.f_genres, textListView);
+        this.androidListViewStrings = textListView;
+        this.imagesId = imagesId;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View viewRow = layoutInflater.inflate(R.layout.i_genres, null,
+                true);
+        TextView mtextView = (TextView) viewRow.findViewById(R.id.text_view);
+        ImageView mimageView = (ImageView) viewRow.findViewById(R.id.image_view);
+        mtextView.setText(androidListViewStrings[i]);
+        mimageView.setImageResource(imagesId[i]);
+        return viewRow;
+    }
+}
---
+++ app/src/main/java/com/sdm/sergio/mytrack/fragment/FragmentProfileWeb.java
@@ -0,0 +1,111 @@
+package com.sdm.sergio.mytrack.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.sdm.sergio.mytrack.R;
+
+public class FragmentProfileWeb extends Fragment {
+    private WebView mWebView;
+    private String url ="https://trakt.tv/auth/signin/";
+    private ProgressBar bar;
+    private View v;
+
+    private ProgressDialog progress;
+    private WebView webView;
+
+    public static FragmentProfileWeb newInstance() {
+        FragmentProfileWeb fragment = new FragmentProfileWeb();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View mainView = (View) inflater.inflate(R.layout.f_profile_web, container, false);
+        WebView webView = (WebView)mainView.findViewById(R.id.webview);
+        webView.setWebViewClient(new MantenerDominioWeb());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        webView.setBackgroundResource(R.color.colorPrimaryDark);
+        webView.canGoBack();
+        webView.goBack();
+        webView.loadUrl("https://trakt.tv/auth/signin/");
+
+        progress = ProgressDialog.show(getContext(), "Aguarde..",
+                "Conectando con Track.tv.", true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                if (progress != null)
+                    progress.dismiss();
+            }
+        });
+        return mainView;
+    }
+    public class MantenerDominioWeb extends WebViewClient {
+
+        private WebView webView;
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().endsWith("trakt.tv")) {
+                return false;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            view.getContext().startActivity(intent);
+            return true;
+        }
+
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
+
+}
