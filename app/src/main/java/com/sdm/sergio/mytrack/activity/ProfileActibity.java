908b702f3958c258214e226e473ba8f4c55d479b
Ines <agnes19gomez@gmail.com>
2017-04-24 15:48:51 +0200

    vista profile usuario, vista registro, vista pelicula

---
+++ app/src/main/java/com/sdm/sergio/mytrack/activity/ProfileActibity.java
@@ -0,0 +1,35 @@
+package com.sdm.sergio.mytrack.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sdm.sergio.mytrack.R;
+
+
+public class ProfileActibity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        //Ir a la vista de perfil del usuario
+        setContentView(R.layout.a_profile);
+
+    }
+
+    //Rellenar datos del usuario
+    public void rellenarDatos(){
+
+        //Nombre del Usuario
+        TextView nombreUser = (TextView) findViewById(R.id.user_profile_name);
+
+        //Imagen del Usuario
+        ImageView imagenUser = (ImageView) findViewById(R.id.user_profile_photo);
+
+
+
+        }
+
+    }
